0f389de11fd23b4de3ecb1764429877aac46befc
Remi <remonier@ensc.fr>
2025-04-07 12:05:56 +0200

    TestEncore

--- projet/obj/Debug/net8.0/projet.AssemblyInfo.cs
+++ projet/obj/Debug/net8.0/projet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83621c5048b59d1cf0f2277119568e18d7404d7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e86028b88492325789857a41c5797e42db70a3d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("projet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
